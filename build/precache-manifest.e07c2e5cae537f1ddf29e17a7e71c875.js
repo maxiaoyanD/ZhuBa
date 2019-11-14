c15f3b2c1cb0b9fcc56e693c235a7f982eef7f55
maxiaoyanD <2390140586@qq.com>
2019-11-14 10:14:26 +0800

    住吧APP

---
+++ build/precache-manifest.e07c2e5cae537f1ddf29e17a7e71c875.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809815dc1816ed2a418f4f8f18200bf6",
+    "url": "index.html"
+  },
+  {
+    "revision": "57004ea73b07dc46e2f0",
+    "url": "static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "2321a1bd0906ae1ef90d",
+    "url": "static/css/main.e2e965f7.chunk.css"
+  },
+  {
+    "revision": "57004ea73b07dc46e2f0",
+    "url": "static/js/2.094f43c4.chunk.js"
+  },
+  {
+    "revision": "2321a1bd0906ae1ef90d",
+    "url": "static/js/main.c3e3ac79.chunk.js"
+  },
+  {
+    "revision": "51cb8bceaed3cd140961",
+    "url": "static/js/runtime-main.805461b1.js"
+  },
+  {
+    "revision": "93f1150be43cbaaf39f5da3819472e20",
+    "url": "static/media/back.93f1150b.png"
+  },
+  {
+    "revision": "7cce30f3791fe4dc1014a9bdb699f70e",
+    "url": "static/media/banner-1.7cce30f3.png"
+  },
+  {
+    "revision": "c1cd832b6cb74c3ab1bc8dd0fbec38b7",
+    "url": "static/media/banner-2.c1cd832b.png"
+  },
+  {
+    "revision": "1c07f0433da0f2f0682701b457af71ab",
+    "url": "static/media/lg-1.1c07f043.png"
+  },
+  {
+    "revision": "a6240527a99bb5b5914143532e5dc802",
+    "url": "static/media/lg-2.a6240527.png"
+  },
+  {
+    "revision": "dd6208b95128f101ff2230f58e3dfe4e",
+    "url": "static/media/lg-3.dd6208b9.png"
+  },
+  {
+    "revision": "a1c20c3e568305cc3029d57ab55937ca",
+    "url": "static/media/lg-4.a1c20c3e.png"
+  },
+  {
+    "revision": "1c07f0433da0f2f0682701b457af71ab",
+    "url": "static/media/lg-5.1c07f043.png"
+  },
+  {
+    "revision": "a6240527a99bb5b5914143532e5dc802",
+    "url": "static/media/lg-6.a6240527.png"
+  },
+  {
+    "revision": "35c0ef0f15c408d9742679b041a90764",
+    "url": "static/media/m-1.35c0ef0f.png"
+  },
+  {
+    "revision": "d2c0010e52ba3866e470938e9bd06e49",
+    "url": "static/media/m-2.d2c0010e.png"
+  },
+  {
+    "revision": "e42373a2767d30ccbfa27dbbaf67c790",
+    "url": "static/media/m-3.e42373a2.png"
+  },
+  {
+    "revision": "06610925441a878f7e6a554b0e27ab37",
+    "url": "static/media/store-banner1.06610925.png"
+  },
+  {
+    "revision": "100c5a4eb3081b08257924c2294bd03a",
+    "url": "static/media/store-d1.100c5a4e.png"
+  },
+  {
+    "revision": "db695d7d4484d0b5cfdf5bf6350a7800",
+    "url": "static/media/store-d2.db695d7d.png"
+  },
+  {
+    "revision": "93738ff9cf67fd9d2126029a4fa986d6",
+    "url": "static/media/store-d3.93738ff9.png"
+  }
+]);
